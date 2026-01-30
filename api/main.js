97187a7889b106eed50f22c8e04e9591f62fae30
lordStrider <squallion26@gmail.com>
2026-01-30 01:00:18 -0300

    Deixando na tela de login provisorio

--- api/main.js
+++ api/main.js
@@ -3,5 +3,5 @@ import { templateAdmin } from "./pages/templateAdmin.js";
 window.onload = function() {
     console.log("Página totalmente carregada (incluindo imagens)!");
     // Seu código aqui
-    templateAdmin()
+    templateHome()
 };
