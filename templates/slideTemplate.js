57eb6bc9746eb849767153782dbd9ac36c4795d2
lordStrider <squallion26@gmail.com>
2026-02-01 16:27:02 -0300

    Criando a area sobre nós
    Atualizando a site
    *Area sobre nós
    *Criando configuração para o slideShow

--- templates/slideTemplate.js
+++ templates/slideTemplate.js
@@ -1,26 +1,8 @@
+import { sliderConfig } from "./sliderConfig.js";
 export const slideTemplate = ()=> {
-  const template =  `<div class="sp-slides">
-        <!-- Slide 1 -->
-        <div class="sp-slide">
-        <div class="logo-mark"><img src="images/logo-brave.png"></div>
-            <p class="sp-layer"><span>Divirta-se viajando pelo mundo dos sonhos.</span></p>
-            <img class="sp-image" src="img/brave-dream-slide.jpg">
-        </div>
-
-        <!-- Slide 2 -->d
-        <div class="sp-slide">
-            <img class="sp-image" src="img/zennowslide.jpg">
-        </div>
-
-        <!-- Slide 3 -->
-        <div class="sp-slide">
-            <img class="sp-image" src="img/bibliaquiz-slide.jpg">
-            <h3 class="sp-layer">Lorem ipsum dolor sit amet</h3>
-        </div>
-    </div>   
-`;
+ 
     const sliderPre = document.querySelector(".slider-pro");
-    sliderPre.innerHTML = template;
+    sliderPre.innerHTML = sliderConfig();
 
     const slider = new SliderPro('#my-slider', {
             autoplayDirection: 'normal',
@@ -33,7 +15,7 @@ export const slideTemplate = ()=> {
             thumbnailWidth: 200,
             thumbnailHeight: 100,
             thumbnailPointer: true,
-            autoplay: false,
+            autoplay: true,
             autoplayDelay: 3000,
             fade: true,
             fadeDuration: 1500,
---
+++ templates/sliderConfig.js
@@ -0,0 +1,39 @@
+export const sliderConfig = ()=> {
+    let mySlider = "";
+    const sliderContent = [
+        {
+            "title": "BRAVE DREAM GUARDIAN",
+            "logoTitle": "images/logo-brave.png",
+            "image": "img/brave-dream-slide.jpg",
+            "description": "<span>Divirta-se viajando pelo mundo dos sonhos.</span>",
+        },
+        {
+            "title": "ZEN NOW",
+            "logoTitle": "images/logo-zennow.png",
+            "image": "img/zennowslide.jpg",
+            "description": "<span>Relaxe e medite ao som da natureza e sons relaxantes.</span>",
+        },
+        {
+            "title": "BIBLIA QUIZ",
+            "logoTitle": "images/logo-biblequiz.png",
+            "image": "img/bibliaquiz-slide.jpg",
+            "description": "<span>Relache e divirta-se mostrando seus conhecimentos Bíblicos</span>",
+        },
+    ]
+    sliderContent.forEach((e, index) => {
+        mySlider += `
+        <!-- Slide ${index + 1} -->
+        <div class="sp-slide">
+            <div class="logo-mark"><img src="${e.logoTitle}"><p> ${e.title}</p></div>
+            <p class="sp-layer"><span>${e.description}</span></p>
+            <img class="sp-image" src="${e.image}">
+        </div>
+        `;
+    });
+    let criandoSlider = `
+        <div class="sp-slides">
+        ${mySlider}
+        </div>
+    `; 
+    return criandoSlider;
+}
