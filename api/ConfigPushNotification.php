e44e3febea52df34aaf43c52a1f56721d71ebb01
lordStrider <squallion26@gmail.com>
2026-02-17 19:41:02 -0300

    Atualizado validação push com autenticação do token

--- api/ConfigPushNotification.php
+++ api/ConfigPushNotification.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once 'validar.php';
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
--- api/modulos/createPush.js
+++ api/modulos/createPush.js
@@ -4,7 +4,7 @@ export const createPush = () => {
     const corpo = document.querySelector("#corpo");
     const imagem = document.querySelector("#imagem");
     const token = document.querySelector("#token");
-
+    const authToken = localStorage.getItem("jwt_token");
     const previewTitle = document.getElementById('previewTitle');
     const previewText = document.getElementById('previewText');
     const previewImage = document.getElementById('previewImage');
@@ -21,6 +21,9 @@ export const createPush = () => {
             url: "https://api.dreamsorcererstudios.com.br/ConfigPushNotification.php",
             type: 'POST',
             data: dados,
+            headers: {
+                'Authorization': 'Bearer ' + authToken
+            },
 
             success: function (response) {
                 try {
@@ -42,25 +45,25 @@ export const createPush = () => {
 
     // aqui codigo para atualizar imagens e textos
     // Título
-  titulo.addEventListener('input', () => {
-    previewTitle.textContent =
-      titulo.value || 'Título da notificação';
-  });
+    titulo.addEventListener('input', () => {
+        previewTitle.textContent =
+            titulo.value || 'Título da notificação';
+    });
 
-  // Texto
-  corpo.addEventListener('input', () => {
-    previewText.textContent =
-      corpo.value || 'Texto da notificação';
-  });
+    // Texto
+    corpo.addEventListener('input', () => {
+        previewText.textContent =
+            corpo.value || 'Texto da notificação';
+    });
 
-  // Imagem
-  imagem.addEventListener('input', () => {
-    previewImage.src =
-      imagem.value || 'https://cdn-icons-png.flaticon.com/512/4712/4712109.png';
-  });
+    // Imagem
+    imagem.addEventListener('input', () => {
+        previewImage.src =
+            imagem.value || 'https://cdn-icons-png.flaticon.com/512/4712/4712109.png';
+    });
 
-  // Fallback se a imagem quebrar
-  previewImage.onerror = () => {
-    previewImage.src = 'https://cdn-icons-png.flaticon.com/512/4712/4712109.png';
-  };
+    // Fallback se a imagem quebrar
+    previewImage.onerror = () => {
+        previewImage.src = 'https://cdn-icons-png.flaticon.com/512/4712/4712109.png';
+    };
 }
